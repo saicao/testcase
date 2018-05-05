6a229a6dde62b58609f8e17a1e4c41b7b5c605c5
SaiCao <saicao@buffalo.edu>
2018-05-05 12:29:48 -0400

    testcase

---
+++ A7/testcode.cpp
@@ -0,0 +1,21 @@
+#include <string>
+int testcode(){
+  
+
+    std::string q []={"WILL","BILL","BILL","DILL","SANTA","SANTA","JN","AJON","JONA","BDFA","ACP"};
+    for(int i=0;i<11;++i){
+        //your print code
+        print(q[i],dic);
+
+
+
+    }
+  
+ 
+
+
+
+
+
+
+}
